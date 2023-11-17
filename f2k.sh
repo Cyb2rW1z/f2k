8945d089240a35007d0e1ccbce67cf5e4331529a
Jbr AKA user32.dll <126031099+Cyb2rW1z@users.noreply.github.com>
2023-11-17 06:12:13 -0500

    Update f2k.sh

--- f2k.sh
+++ f2k.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
-# Define the host and paths files
 hosts_file="hosts"
 paths_file="paths"
-
-# Output directory
 output_dir="outputs"
 
-# Create the output directory if it doesn't exist
 mkdir -p "$output_dir"
 
-# Check if hosts and paths files exist
 if [ ! -f "$hosts_file" ] || [ ! -f "$paths_file" ]; then
     echo "Error: hosts or paths file not found."
     exit 1
 fi
 
-# Loop through each line in the hosts file
 while IFS= read -r host; do
-    # Loop through each line in the paths file
     while IFS= read -r path; do
-        # Construct the URL
         url="$host/$path"
 
-        # Use curl to send a request and save the output to a file
         output_file="$output_dir/$(echo "$host" | tr -d '/').$(echo "$path" | tr -d '/')"
         response_code=$(curl -s -o "$output_file" -w "%{http_code}" "$url")
 
@@ -33,7 +24,6 @@ while IFS= read -r host; do
             echo "Output saved to: $output_file"
         else
             echo "Request to $url did not return a 200 response code. Response code: $response_code"
-            # Remove the output file if the response code is not 200
             rm -f "$output_file"
         fi
     done < "$paths_file"
